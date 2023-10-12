2a95b748428f78db78c523f7ecd8f9cc17f8530e
UchithmaSenevirathne <uchithma@gmail.com>
2023-10-12 13:39:38 +0530

    setResizable set true

--- src/main/java/lk/ijse/dep/AppInitializer.java
+++ src/main/java/lk/ijse/dep/AppInitializer.java
@@ -16,7 +16,7 @@ public class AppInitializer extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/CreatePlayer.fxml"))));
-        primaryStage.setResizable(false);
+        primaryStage.setResizable(true);
         primaryStage.setTitle("Connect 4 Game - Create Player");
         primaryStage.show();
         primaryStage.centerOnScreen();
